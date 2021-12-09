7aacf5653da37ed85f271c37725ae3de8f31e761
dyon <dyon@users.noreply.github.com>
2021-12-09 18:27:12 +0100

    Update refs to own fork

--- Formula/sshrc.rb
+++ Formula/sshrc.rb
@@ -1,9 +1,9 @@
 class Sshrc < Formula
   desc "Bring your .bashrc, .vimrc, etc. with you when you SSH"
   homepage "https://github.com/ikuwow/sshrc"
-  url "https://github.com/ikuwow/sshrc/archive/0.7.0.tar.gz"
+  url "https://github.com/dyon/sshrc/archive/0.7.0.tar.gz"
   sha256 "09dc3ab22d07674fd7f226de5b16689012d30bda1e95a84354c0902cf10991f8"
-  head "https://github.com/ikuwow/sshrc.git"
+  head "https://github.com/dyon/sshrc.git"
 
   def install
     bin.install %w[sshrc moshrc]
